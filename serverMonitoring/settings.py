7a4406d4ef56be0b5fbacd1c2c2edb8897f4ff55
Daniel D. <126314303+Daniel-Drysdale@users.noreply.github.com>
2025-04-21 12:06:15 -0400

    Updating For Backend Host

--- serverMonitoring/settings.py
+++ serverMonitoring/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'http://localhost:5173', 'https://endpoint-monitor.onrender.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'http://localhost:5173', 'endpoint-monitor.onrender.com']
 
 
 # Application definition
